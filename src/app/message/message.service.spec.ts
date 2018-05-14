348e71fe847ff2bc573b993ad34257edb8ef65cd
Andrii Skrebets <andrii.skrebets@magnise.com>
2018-05-14 11:31:02 +0300

    add new message

--- src/app/message/message.service.spec.ts
+++ src/app/message/message.service.spec.ts
@@ -1,59 +1,55 @@
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
 
-import { MessagesService } from './messages.service';
+import { MessagesService } from "./messages.service";
 
-import { Message } from './message.model';
-import { Thread } from '../thread/thread.model';
-import { User } from '../user/user.model';
+import { Message } from "./message.model";
+import { Thread } from "../thread/thread.model";
+import { User } from "../user/user.model";
 
-describe('Message Service', () => {
-  it('should test', () => {
+describe("Message Service", () => {
+  it("should test", () => {
     const user: User = {
       id: v4(),
-      name: 'John',
-      avatarSrc: '',
+      name: "John",
+      avatarSrc: ""
     };
 
     const thread: Thread = {
       id: v4(),
-      name: 'John thread',
-      avatarUrl: '',
+      name: "John thread",
+      avatarUrl: ""
     };
 
     const message1: Message = {
       id: v4(),
       author: user,
       sendAt: new Date(),
-      text: 'hi',
+      text: "hi",
       thread: thread,
-      isRead: false,
+      isRead: false
     };
 
     const message2: Message = {
       id: v4(),
       author: user,
       sendAt: new Date(),
-      text: 'Bye',
+      text: "Bye",
       thread: thread,
-      isRead: false,
+      isRead: false
     };
 
     const messageService = new MessagesService();
 
-    messageService.newMessage$.subscribe(
-      (newMessage: Message) => {
-        console.log('=> new messages', newMessage.text);
-      }
-    );
+    messageService.newMessage$.subscribe((newMessage: Message) => {
+      console.log("=> new messages", newMessage.text);
+    });
 
-    messageService.message$.subscribe(
-      (messages: Message[]) => {
-        console.log('=> messages length', messages.length);
-      }
-    );
+    messageService.message$.subscribe((messages: Message[]) => {
+      console.log("=> messages length", messages.length);
+    });
 
     messageService.addMessage(message1);
     messageService.addMessage(message2);
-
+    messageService.addMessage(message2);
   });
 });
