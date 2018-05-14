2b2c03c23c6787dc5b8cf4a8a3b0c08755de661f
Andrii Skrebets <andrii.skrebets@magnise.com>
2018-05-14 11:39:14 +0300

    add new commit message

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
+import { Subject } from "rxjs/Subject";
+import { Observable } from "rxjs/Observable";
+import "rxjs/Rx";
 
-import { Message } from './message.model';
-import { Thread } from '../thread/thread.model';
-import { User } from '../user/user.model';
+import { Message } from "./message.model";
+import { Thread } from "../thread/thread.model";
+import { User } from "../user/user.model";
 
 type MessagesOperation = (messages: Message[]) => Message[];
 
@@ -22,33 +22,35 @@ export class MessagesService {
   markThreadAsRead$ = new Subject<any>();
 
   constructor() {
-    this.message$ = this.update$.scan(
-      (messages: Message[], operation: MessagesOperation) =>
-        operation(messages),
-      initialMessages
-    )
-    .shareReplay(1);
+    this.message$ = this.update$
+      .scan(
+        (messages: Message[], operation: MessagesOperation) =>
+          operation(messages),
+        initialMessages
+      )
+      .shareReplay(1);
     // .publishReplay(1)
     // .refCount();
-    this.create$.map((message: Message): MessagesOperation =>
-      (messages: Message[]) =>
+    this.create$
+      .map((message: Message): MessagesOperation => (messages: Message[]) =>
         messages.concat(message)
-    )
-    .subscribe(this.update$);
+      )
+      .subscribe(this.update$);
 
     this.newMessage$.subscribe(this.create$);
 
-    this.markThreadAsRead$.map((thread: Thread): MessagesOperation => {
-      return (messages: Message[]) => {
-        return messages.map((message: Message) => {
-          if (message.thread.id === thread.id) {
-            message.isRead = true;
-          }
-          return message;
-        });
-      };
-    })
-    .subscribe(this.update$);
+    this.markThreadAsRead$
+      .map((thread: Thread): MessagesOperation => {
+        return (messages: Message[]) => {
+          return messages.map((message: Message) => {
+            if (message.thread.id === thread.id) {
+              message.isRead = true;
+            }
+            return message;
+          });
+        };
+      })
+      .subscribe(this.update$);
   }
 
   // add a new message
@@ -56,14 +58,14 @@ export class MessagesService {
     this.newMessage$.next(message);
   }
 
+  // meekfwbiefhnkwessages from passed thread and not authored by user
   // messages from passed thread and not authored by user
   messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
-    return this.newMessage$.filter((message: Message) =>
-      (message.thread.id === thread.id) && (message.author.id !== user.id)
+    return this.newMessage$.filter(
+      (message: Message) =>
+        message.thread.id === thread.id && message.author.id !== user.id
     );
   }
 }
 
-export const messageServiceInjectables: Array<any> = [
-  MessagesService,
-];
+export const messageServiceInjectables: Array<any> = [MessagesService];
